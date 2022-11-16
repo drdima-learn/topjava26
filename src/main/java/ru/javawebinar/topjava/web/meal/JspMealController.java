36997e8e1be38bdc96a3e84a936a6a5d68ede9b6
drdima <drdima2@gmail.com>
2022-11-16 11:56:36 -0600

    JspMealController small refactoring

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -5,23 +5,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
+@RequestMapping("/meals")
 public class JspMealController {
     private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
 
     @Autowired
     private MealRestController mealController;
 
-    @GetMapping("/meals")
+    @GetMapping
     public String getMeals(Model model,
                            @RequestParam(value = "startDate", required = false) String startDate,
                            @RequestParam(value = "endDate", required = false) String endDate,
@@ -39,27 +37,27 @@ public class JspMealController {
         return "meals";
     }
 
-    @GetMapping("/meals/delete")
+    @GetMapping("/delete")
     public String deleteMeal(@RequestParam int id) {
         mealController.delete(id);
         return "redirect:/meals";
     }
 
-    @GetMapping("/meals/update")
+    @GetMapping("/update")
     public String showMealUpdateForm(Model model, @RequestParam("id") int id) {
         model.addAttribute("action", "update");
         model.addAttribute("meal", mealController.get(id));
         return "mealForm";
     }
 
-    @GetMapping("/meals/create")
+    @GetMapping("/create")
     public String showMealCreateForm(Model model) {
         model.addAttribute("action", "create");
         model.addAttribute("meal", new Meal());
         return "mealForm";
     }
 
-    @PostMapping("/meals/update")
+    @PostMapping("/update")
     public String updateMeal(@ModelAttribute("meal") Meal meal) {
         log.info("/meals/update   meal={}", meal);
         if (meal.isNew()) {
