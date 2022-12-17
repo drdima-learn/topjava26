033242cb85f246ed4c2f643b420abb01a20404c6
drdima <drdima2@gmail.com>
2022-12-17 17:50:32 -0600

    small refactoring2

--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -1,16 +1,12 @@
 package ru.javawebinar.topjava.web;
 
-import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
-import ru.javawebinar.topjava.model.User;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static ru.javawebinar.topjava.UserTestData.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ResourceControllerTest extends AbstractControllerTest {
     @Test
@@ -20,6 +16,7 @@ public class ResourceControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/css;charset=UTF-8"))
                 .andExpect(content().contentTypeCompatibleWith("text/css"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.valueOf("text/css")))
         ;
     }
 }
